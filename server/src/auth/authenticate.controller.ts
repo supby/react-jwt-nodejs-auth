6234f0e0b49b8b037f285283de972e5c03154fe8
Andre Suzanovich <supius@gmail.com>
2019-10-05 13:44:33 +0200

    use user in auth serv

--- server/src/auth/authenticate.controller.ts
+++ server/src/auth/authenticate.controller.ts
@@ -1,14 +1,21 @@
 import express = require('express');
+import { IUser } from '../users/user';
+import { authenticate } from './authenticate.service';
 
 const router = express.Router();
 
 // routes
 router.post('/', (req, res, next) => {
-    console.log('authenticate request');
-    
+  console.log('authenticate request');
+  console.log(req.body);
+//   userService
+//     .authenticate(req.body)
+//     .then(user: IUser =>
+//       user
+//         ? res.json(user)
+//         : res.status(400).json({ message: "Username or password is incorrect" })
+//     )
+//     .catch((err: any) => next(err));
 });
 
 export default router;
-
-
-
