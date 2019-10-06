cfe0d7238b2f65f73a2e1b23bdbdd57d83a1bbd1
Andre Suzanovich <supius@gmail.com>
2019-10-06 12:03:27 +0200

    fix routing

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,8 +16,8 @@ app.use(cors());
 app.use(errorHandler);
 
 // api routes
-app.post('/authenticate', authenticateController); // public
-app.get('/data', authenticateMiddleware, dataController); // private
+app.use('/authenticate', authenticateController); // public
+app.use('/data', authenticateMiddleware, dataController); // private
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
